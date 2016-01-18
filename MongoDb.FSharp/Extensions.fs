fe33984838d19767d2d0499d790bb5150634c22b
Ilya Titkov <ilya@titkov.me>
2016-01-18 21:38:24 +0500

    Make toList better

--- MongoDb.FSharp/Extensions.fs
+++ MongoDb.FSharp/Extensions.fs
@@ -6,12 +6,6 @@ type IFindFluent<'TDoc, 'TProj> with
 
     member x.ToFSharpListAsync() = 
         async { 
-            let! cursor = x.ToCursorAsync() |> Async.AwaitTask
-            let rec toList list = 
-                async { 
-                    let! moved = cursor.MoveNextAsync() |> Async.AwaitTask
-                    if (not moved) then return list
-                    else return! toList (List.append list <| Seq.toList cursor.Current)
-                }
-            return! toList []
+            let! cSharpList = x.ToListAsync() |> Async.AwaitTask
+            return Seq.toList cSharpList
         }
